28d558435757bdc7e69e028a55635349f0a40626
tokfrans03 <34898241+tokfrans03@users.noreply.github.com>
2019-11-18 19:55:54 +0100

    default conf w/ help page

--- abbexpectmore/src/views/Home.vue
+++ abbexpectmore/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ControlLights/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ControlLights from '@/components/ControlLights.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    ControlLights
   }
 }
 </script>
