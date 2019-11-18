4cc19cd688f799c1298a5868e4ee4870a967223d
tokfrans03 <34898241+tokfrans03@users.noreply.github.com>
2019-11-18 17:08:19 +0100

    added deploy and vue prodject

---
+++ deploy.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+echo "##############################"
+echo
+echo "installing..."
+echo
+echo "##############################"
+npm i
+echo "##############################"
+echo
+echo "Building..."
+echo
+echo "##############################"
+npm run build
+
+echo "##############################"
+echo
+echo "Done!"
+echo
+echo "##############################"
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+echo "##############################"
+echo
+echo "Commiting..."
+echo
+echo "##############################"
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:flexienok/AbbExpectMore.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:flexienok/flexienok.github.io.git master:master
+
+cd -
